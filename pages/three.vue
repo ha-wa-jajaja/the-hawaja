3f51aafa3c5d977f1386208f4fb315ad658231c6
Jeffrey He <jeffrey@digisalad.cool>
2023-11-29 11:36:59 +0800

    three wip

---
+++ pages/three.vue
@@ -0,0 +1,98 @@
+<template>
+    <div>
+        <canvas ref="canvas" />
+    </div>
+</template>
+<script setup lang="ts">
+import {
+    Scene,
+    PerspectiveCamera,
+    Mesh,
+    SphereGeometry,
+    MeshBasicMaterial,
+    WebGLRenderer,
+    Color,
+    Fog,
+    AmbientLight,
+    DirectionalLight,
+    HemisphereLight,
+} from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+import { useWindowSize } from "@vueuse/core";
+const { width, height } = useWindowSize();
+const aspectRatio = computed(() => {
+    return width.value / height.value;
+});
+
+let renderer: WebGLRenderer;
+let controls: OrbitControls;
+const canvas: Ref<HTMLCanvasElement | null> = ref(null);
+
+const bgc = new Color("#FFFFFF");
+const scene = new Scene();
+scene.fog = new Fog(bgc, 0.1, 75);
+scene.background = bgc;
+
+const camera = new PerspectiveCamera(
+    75,
+    aspectRatio.value,
+    0.1,
+    1000
+);
+camera.position.set(0, 2, 4);
+
+scene.add(camera);
+const ambientLight = new AmbientLight(0xffffff, 2.5);
+scene.add(ambientLight);
+const dirLight = new DirectionalLight(0xffffff, 2.5);
+scene.add(dirLight);
+
+const gltfLoader = new GLTFLoader();
+gltfLoader.load("/spinning_top/scene.gltf", (gltf) => {
+    scene.add(gltf.scene);
+});
+
+function updateRenderer() {
+    renderer.setSize(width.value, height.value);
+    renderer.render(scene, camera);
+}
+function UpdateCamera() {
+    camera.aspect = aspectRatio.value;
+    camera.updateProjectionMatrix();
+}
+watch(aspectRatio, () => {
+    UpdateCamera();
+    updateRenderer();
+});
+
+function setRenderer() {
+    if (!canvas.value) return;
+    renderer = new WebGLRenderer({
+        canvas: canvas.value,
+        alpha: true,
+    });
+    renderer.setPixelRatio(
+        Math.min(window.devicePixelRatio, 2)
+    );
+
+    controls = new OrbitControls(
+        camera,
+        renderer.domElement
+    );
+    updateRenderer();
+}
+
+onMounted(() => {
+    setRenderer();
+    loop();
+});
+
+const loop = () => {
+    controls.update();
+    renderer.render(scene, camera);
+    requestAnimationFrame(loop);
+};
+</script>
+<style lang=""></style>
