2085303d94646731fa66f3b7d141c11579385c01
Jeffrey He <jeffrey@digisalad.cool>
2023-12-25 09:29:04 +0800

    grid update

---
+++ components/home/skills/skill-gird.vue
@@ -0,0 +1,39 @@
+<template>
+    <div
+        class="w-full grid grid-cols-6 grid-rows-5 h-4/5 gap-4"
+    >
+        <div
+            class="row-span-5 col-span-1 bg-green-500"
+        ></div>
+        <div
+            class="row-span-3 col-span-1 bg-green-400"
+        ></div>
+        <div class="row-span-2 col-span-1 bg-red-400"></div>
+        <div
+            class="row-span-2 col-span-1 bg-blue-400"
+        ></div>
+        <div
+            class="row-span-2 col-span-2 bg-yellow-400"
+        ></div>
+        <div
+            class="row-span-1 col-span-3 bg-green-600"
+        ></div>
+        <div
+            class="row-span-3 col-span-1 bg-[#FC6464]"
+        ></div>
+        <div
+            class="row-span-1 col-span-3 bg-blue-600"
+        ></div>
+        <div
+            class="row-span-2 col-span-1 bg-slate-300"
+        ></div>
+        <div
+            class="row-span-1 col-span-2 bg-[#64C5FC]"
+        ></div>
+        <div
+            class="row-span-1 col-span-1 bg-white border-black border"
+        ></div>
+    </div>
+</template>
+<script setup></script>
+<style lang=""></style>
--- pages/test-2.vue
+++ pages/test-2.vue
@@ -1,42 +1,7 @@
 <template>
-    <div>
-        <div
-            class="w-screen h-screen bg-white text-black text-[100px] flex items-center justify-center"
-        >
-            enter
-        </div>
-        <UiStickyPinSection :height="300" ref="pinWrapper">
-            <UiExpandCircle
-                ref="circle"
-                :circle-id="'test-2'"
-            />
-            <div class="text-black text-[100px]">
-                NOT RELLAX
-            </div>
-        </UiStickyPinSection>
-        <div
-            class="w-screen h-screen bg-white text-black text-[100px] flex items-center justify-center"
-        >
-            end
-        </div>
+    <div class="container h-screen">
+        <HomeSkillsSkillGird />
     </div>
 </template>
-<script setup>
-const pinWrapper = ref(null);
-const scrollProgress = computed(
-    () => pinWrapper.value?.progress
-);
-
-const circle = ref(null);
-const circleClass = computed(() => circle.value.uniqueId);
-
-watch(scrollProgress, (val) => {
-    useTriggerRellax(
-        "." + circleClass.value,
-        val,
-        0.7,
-        -0.5
-    );
-});
-</script>
+<script setup></script>
 <style lang=""></style>
